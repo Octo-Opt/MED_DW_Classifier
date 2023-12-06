15b71e47eb38662e66dd303b97396f56a351360a
MinLee0210 <bastreet124@gmail.com>
2023-12-06 19:56:46 +0700

    In Progress: Stuck with building Graph

--- utils.py
+++ utils.py
@@ -1,12 +1,9 @@
-import yaml, json
+import yaml
 
+
+#   ----- READ FILES -----
 def yaml_read(dir): 
     content = yaml.load(open(dir, 'r'), Loader=yaml.FullLoader)
     return content
 
-def json_load(dir): 
-    content = json.load(open(dir, 'r'))
-    return content
-
-def json_dump(data, dir):
-    json.dump(data, open(dir, 'w')) 
+#   ----- 
